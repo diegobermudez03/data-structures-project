48928aa1e78b1b5b4b9f05548542f2a48136f70e
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-07-28 14:23:08 -0500

    changed the structure, so that it know splits the command in a list, and it's easier to check the parts of the command with that list

--- menu.cxx
+++ menu.cxx
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <list>
 
 void enter_command(std::string& command);
-void cargar_command(std::string& command, short initial_index);
-void listado_command(std::string& command, short initial_index);
-void envolvente_command(std::string& command, short initial_index);
-void descargar_command(std::string& command, short initial_index);
-void guardar_command(std::string& command, short initial_index);
-void v_cercano_command(std::string& command, short initial_index);
-void v_cercanos_caja_command(std::string& command, short initial_index);
-void ruta_corta_command(std::string& command, short initial_index);
-void ruta_corta_centro_command(std::string& command, short initial_index);
-void ayuda_command(std::string& command, short initial_index);
+
+void cargar_command(std::list<std::string>* words);
+void listado_command(std::list<std::string>* words);
+void envolvente_command(std::list<std::string>* words);
+void descargar_command(std::list<std::string>* words);
+void guardar_command(std::list<std::string>* words);
+void v_cercano_command(std::list<std::string>* words);
+void v_cercanos_caja_command(std::list<std::string>* words);
+void ruta_corta_command(std::list<std::string>* words);
+void ruta_corta_centro_command(std::list<std::string>* words);
+void ayuda_command(std::list<std::string>* words);
+
 void fill_help_commands();
 std::string substring(std::string& command, int in_pos, int end_pos);
+std::list<std::string>* split(std::string&command);
 int char_finder(std::string& command, char end_char, int begin_pos);
 
 std::unordered_map<std::string, std::string> help_map;
 
+//Entry point of the program, simply prints the entry command char $ and listens for inputs
 int main(int argsn, char** args){
     fill_help_commands();
     std::string command;
@@ -29,10 +34,12 @@ int main(int argsn, char** args){
     }
 }
 
+//logic redirection of commands, it redirects each command to its correspondent controller
 void enter_command(std::string& command){
-    short initial_index = char_finder(command, ' ',0);
-    std::string first_word = command.substr(0, initial_index);
-    void (*command_type)(std::string&, short);
+    std::list<std::string>* words = split(command);
+    std::string first_word = words->back(); //gets the initial command
+    words->pop_back();  //pops the command from the list
+    void (*command_type)(std::list<std::string>*);
     if(first_word == "cargar") command_type = cargar_command;
     else if(first_word == "listado") command_type =  listado_command;
     else if(first_word == "envolvente") command_type = envolvente_command;
@@ -47,70 +54,80 @@ void enter_command(std::string& command){
     else{
         std::cout << "\ncomando invalido";
     }
-    if(command_type != nullptr) command_type(command, initial_index);
+
+    /*std::list<std::string>::iterator it = words->begin();
+    for(; it != words->end();++it){
+        std::cout << "\n"<< *it;
+    }*/
+    if(command_type != nullptr) command_type(words);
+    delete words;
 }
 
-void ayuda_command(std::string& command, short initial_index){
-    short end_pos = char_finder(command, ' ', initial_index + 1);
-    std::string word;
-    try{
-        word = command.substr(initial_index +1, end_pos - initial_index);
-    }catch(const std::exception& e){}
-    if(word.empty()){
+//controller for help command of any type, if any type was given, it shows all the commands help
+void ayuda_command(std::list<std::string>* words){
+    if(words->size() == 0){
         std::unordered_map<std::string, std::string>::iterator it = help_map.begin();
         for(it = help_map.begin(); it != help_map.end(); ++it){
             std::cout << "\n  " << it->first << " \n" << it->second << "\n";
         }
         return;
     }
+    std::string word = words->back();
     std::string help;
     help = help_map[word];
     if(help.empty()) help = "comando no existe";
     std::cout << "\n" << help;
 }
 
-void cargar_command(std::string& command, short initial_index){
-    std::string file_name = substring(command, initial_index+1, -1);
-    if(file_name.empty()){
+void cargar_command(std::list<std::string>* words){
+    //it should only be 1 word, so if there'are less or more, then it's worng
+    if(words->size() != 1){
         std::cout << "\nComando invalido\n" << help_map["cargar"];
         return;
     }
-    //TO DO IN NEXT 
+    //TO DO NEXT 
     std::cout << "\nComando valido";
 }
 
-void listado_command(std::string& command, short initial_index){
+void listado_command(std::list<std::string>* words){
+    //there shouldn't be any words at all, so if there are, the command it's wrong
+    if(words->size() != 0){
+        std::cout << "\nComando valido\n" << help_map["listado"];
+        return;
+    }
+    //TO DO NEXT
+    std::cout << "\nComando valido";;
+}
+
+void envolvente_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void envolvente_command(std::string& command, short initial_index){
+void descargar_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void descargar_command(std::string& command, short initial_index){
+void guardar_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void guardar_command(std::string& command, short initial_index){
+void v_cercano_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void v_cercano_command(std::string& command, short initial_index){
+void v_cercanos_caja_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void v_cercanos_caja_command(std::string& command, short initial_index){
+void ruta_corta_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void ruta_corta_command(std::string& command, short initial_index){
+void ruta_corta_centro_command(std::list<std::string>* words){
     std::cout << "\nComando valido";
 }
 
-void ruta_corta_centro_command(std::string& command, short initial_index){
-    std::cout << "\nComando valido";
-}
-
+//function called at the very beginning of the program, it fills a hashmap with all the possible help commands and its correspondent help description
 void fill_help_commands(){
     help_map["cargar"] = "  cargar 'nombre_archivo'\n    carga el objeto a memoria";
     help_map["listado"] = "  listado\n    lista los objetos cargados en memoria";
@@ -141,4 +158,21 @@ int char_finder(std::string& command, char end_char, int begin_pos){
         }
     }
     return command.size();
+}
+
+std::list<std::string>* split(std::string&command){
+    std::list<std::string>* words = new std::list<std::string>;
+    int starting = 0;
+    int ending = 0;
+    while(true){
+        try{
+            ending = char_finder(command, ' ', starting);
+            std::string word = command.substr(starting, ending-starting);
+            if(!word.empty()) words->push_front(word);
+            starting = ending+1;
+        }catch(const std::exception& e){
+            break;
+        }
+    }
+    return words;
 }
