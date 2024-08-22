2ab24db65783078438540a9f14b13726426055d7
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-08-21 21:59:47 -0500

    back to using heap for everything, xd, when changed to stack I found some issues with the references, I could work on improve it since using stack is better for performance, but for this time I prefer to pass and go back to what worked

--- data_organization.h
+++ data_organization.h
@@ -2,19 +2,18 @@
 #define __DATA_ORGANIZATION__H__
 #include <vector>
 #include <unordered_map>
-#include <list>
 #include "object_3d.h"
 
 class DataOrganization{
     protected:
-        std::unordered_map<std::string, Object3d> objects;
-        void get_points(Object3d& object, int& x_max, int& x_min, int& y_max, int& y_min, int& z_max, int& z_min, bool first);
+        std::unordered_map<std::string, Object3d*>* objects = new std::unordered_map<std::string, Object3d*>;
+        void get_points(Object3d* object, int& x_max, int& x_min, int& y_max, int& y_min, int& z_max, int& z_min, bool first);
     public:
         short load_file(std::string& file_name, std::list<std::vector<std::string>*>& result);
-        std::vector<Object3d*> get_objects();
-        std::string envolvente(std::string& object_name);
-        bool descargar(std::string& object_name);
-        bool guardar(std::string& object_name, std::string& file_name);
+        std::vector<Object3d*>* get_objects();
+        std::string envolvente(std::string object_name);
+        bool descargar(std::string object_name);
+        bool guardar(std::string object_name, std::string file_name);
         ~DataOrganization();
 };
 
--- object_3d.cxx
+++ object_3d.cxx
@@ -1,11 +1,7 @@
 #include "object_3d.h"
 #include <vector>
 
-Object3d::Object3d(){}
-
-Object3d::~Object3d(){}
-
-void Object3d::assignAttr(std::string& name, std::vector<std::vector<int>>& vertices, std::list<std::vector<int>>& faces){
+Object3d::Object3d(std::string& name, std::vector<std::vector<int>*>* vertices, std::list<std::vector<int>*>* faces){
     this->name = name;
     this->vertices = vertices;
     this->faces = faces;
@@ -18,15 +14,16 @@ void Object3d::assignAttr(std::string& name, std::vector<std::vector<int>>& vert
     //example:
     //  {0, {6,2,8}}
     //  {2, {4,5}}
-    std::list<std::vector<int>>::iterator it = faces.begin();
+    this->lines = new std::unordered_map<int, std::unordered_set<int>*>;
+    std::list<std::vector<int>*>::iterator it = faces->begin();
     //we iterate over the faces
-    for(;it != faces.end(); ++it){
+    for(;it != faces->end(); ++it){
         //we get an iterator for the vector of each face
-        std::vector<int>::iterator inside_it = it->begin();
+        std::vector<int>::iterator inside_it = (*it)->begin();
         int previous;
         bool first = true;
         //we iterate over the vector with vertex of each face
-        for(; inside_it != it->end(); ++inside_it){
+        for(; inside_it != (*it)->end(); ++inside_it){
             //the logic is to compare by pairs, each vertex with the preious one, and then on
             if(!first){
                 //we simply compare the previous vertex with the current one, and depending on which one is higher, we call the add_line method with the correspondent order
@@ -36,8 +33,8 @@ void Object3d::assignAttr(std::string& name, std::vector<std::vector<int>>& vert
                 //if it's the first vertex, then there's no previous vertex yet, but technically, it's the last vertex of the face, so we check with that vertex
                 first = false;
                 //we must substract 1 to the end address to get the actual last position
-                if(*inside_it < *(it->end()-1)) add_line(*inside_it, *(it->end()-1));
-                else add_line(*(it->end()-1), *inside_it);
+                if(*inside_it < *((*it)->end()-1)) add_line(*inside_it, *((*it)->end()-1));
+                else add_line(*((*it)->end()-1), *inside_it);
             }
             //we replace the previous variable with the current, now previous vertex
             previous = *inside_it;
@@ -45,24 +42,30 @@ void Object3d::assignAttr(std::string& name, std::vector<std::vector<int>>& vert
     }
 }
 
+Object3d::~Object3d(){
+    delete this->vertices;
+    delete this->faces;
+    delete this->lines;
+}
+
 std::string Object3d::get_name(){
     return this->name;
 }
 
 int Object3d::get_count_vertices(){
-    return this->vertices.size();
+    return this->vertices->size();
 }
 
 int Object3d::get_count_faces(){
-    return this->faces.size();
+    return this->faces->size();
 }
 
 //this function simply is the one that counts the lines, it iterates over our map of lines, and it accumulates the size of the sets
 int Object3d::get_count_lines(){
-    std::unordered_map<int, std::unordered_set<int>>::iterator it = this->lines.begin();
+    std::unordered_map<int, std::unordered_set<int>*>::iterator it = this->lines->begin();
     int counter = 0;
-    for(; it != this->lines.end(); ++it){
-        counter += it->second.size();
+    for(; it != this->lines->end(); ++it){
+        counter += it->second->size();
         /*std::cout << "\n Vertex " << it->first << " connects with : ";
         std::unordered_set<int>::iterator it2 = it->second->begin();
         for(;it2 != it->second->end(); ++it2){
@@ -75,15 +78,19 @@ int Object3d::get_count_lines(){
 
 //this function is used to add a line to our map structure
 void Object3d::add_line(int key, int value){
-    //we need to get the set with as a pointer to the set, since we want to modify the same set in the map
-    std::unordered_set<int>* set = &(this->lines)[key];
+    //if the key doesn't already exist, then we need to create it, with its correspondent empty set
+    if(this->lines->find(key) == this->lines->end()){
+        (*this->lines)[key] = new std::unordered_set<int>;
+    }
+    //here we are sure that the key will exist, so we continue to insert the vertex to the set
+    std::unordered_set<int>* set = (*this->lines)[key];
     set->insert(value);
 }
 
-std::vector<std::vector<int>> Object3d::get_vertices(){
+std::vector<std::vector<int>*>* Object3d::get_vertices(){
     return this->vertices;
 }
 
-std::list<std::vector<int>> Object3d::get_faces(){
+std::list<std::vector<int>*>* Object3d::get_faces(){
     return this->faces;
 }
